5390eb3a28bfb64722e87dec7bce3d272d50bbfe
lazermagma <masterabdelrahman7@gmail.com>
2024-10-10 07:28:28 +0300

    removed : unnessary import

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import Image from "next/image";
+
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import CountUp from "react-countup";
 import { FloatingWhatsApp } from "react-floating-whatsapp";
